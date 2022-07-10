f2803d5fd9c505e1fd6b8c3857fd09ef0c0f90cd
Amandeep <singh.amandeep5665@gmail.com>
2022-07-10 15:56:06 +0530

    making coin search

--- src/App.js
+++ src/App.js
@@ -19,14 +19,14 @@ function App() {
 
     axios.get(url).then((response)=>{
       setCoins(response.data)
-      console.log(response.data)
+      
     })
   },[url])
   return (  
     <ThemeProvider >
       <Navbar />
       <Routes>
-        <Route path ='/' element={<Home />} />
+        <Route path ='/' element={<Home coins={coins}/>} />
         <Route path ='/signin' element={<Signin />} />
         <Route path ='/signup' element={<Signup />} />
         <Route path ='/account' element={<Account />} />
--- src/components/CoinSearch.jsx
+++ src/components/CoinSearch.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
 
-const CoinSearch = () => {
+const CoinSearch = ({coins}) => {
+
+    console.log(coins)
   return (
-    <div>CoinSearch</div>
+    <div>
+        <div>
+        <h1>Search Crypto </h1>
+        <form >
+            <input type="text" placeholder='Search A Coin' />
+        </form>
+        </div>
+
+        <table>
+            <thead>
+                <tr>
+                    <th></th>
+                    <th>#</th>
+                    <th>Coin</th>
+                    <th></th>
+                    <th>Price</th>
+                    <th>24h</th>
+                    <th>24h Volume</th>
+                    <th>Market</th>
+                    <th>Last 7 Days</th>
+                </tr>
+            </thead>
+        </table>
+    </div>
   )
 }
 
