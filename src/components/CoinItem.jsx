8203c4bc86f78aa6bf5ce9d02d64aeeb725050ec
Amandeep <singh.amandeep5665@gmail.com>
2022-07-10 18:54:35 +0530

    completed coinitem

--- src/components/CoinItem.jsx
+++ src/components/CoinItem.jsx
@@ -1,28 +1,34 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import {BsCoin} from 'react-icons/bs'
 import { SparklinesLine } from 'react-sparklines';
 import { Sparklines,Sparkline } from 'react-sparklines';
+import { ThemeContext } from '../ThemeContext'
 const CoinItem = ({coin}) => {
-  return (
-    <div>
-        <tr>
 
-<td><BsCoin /></td>
-<td>{coin.market_cap_rank}</td>
-<td><div>
-    <img src={coin.image} alt={coin.id} />
-    <p>{coin.name}</p>
-</div></td>
-<td>{coin.symbol}</td>
-<td>{coin.current_price}</td>
-<td>{coin.price_change_percentage_24h}</td>
-<td>{coin.total_volume}</td>
-<td>{coin.market_cap}</td>
-<td><Sparklines data={coin.sparkline_in_7d.price}>
-    <SparklinesLine color="cyan" />
-    </Sparklines></td>
-</tr>
-    </div>
+    const {theme,setTheme} = useContext(ThemeContext)
+  return (
+    
+        <tr className='h-[80px] border-b overflow-hidden text-xl'>
+
+            <td><BsCoin size={30}/></td>
+            <td>{coin.market_cap_rank}</td>
+            <td><div className='flex items-center'>
+                <img src={coin.image} alt={coin.id} className='w-6 mr-2 rounded-full' />
+                <p className='hidden md:table-cell '>{coin.name}</p>
+            </div></td>
+            <td>{coin.symbol.toUpperCase()}</td>
+            <td>$ {coin.current_price.toLocaleString()} </td>
+            <td>{coin.price_change_percentage_24h > 0 ? (<p className='text-green-600'>{coin.price_change_percentage_24h.toFixed(2)}</p>) : (<p className='text-red-600'>{coin.price_change_percentage_24h.toFixed(2)}</p>)}
+                </td>
+            <td className='w-[180px] hidden md:table-cell'>$ {coin.total_volume.toLocaleString()}</td>
+            <td className='w-[180px] hidden md:table-cell'>$ {coin.market_cap.toLocaleString()}</td>
+            <td><Sparklines data={coin.sparkline_in_7d.price}>
+
+            {theme==='dark' ? (<SparklinesLine color="pink" />) :
+                (<SparklinesLine color="#2b6cb0" />)}
+                </Sparklines></td>
+            </tr>
+    
   )
 }
 
--- src/components/CoinSearch.jsx
+++ src/components/CoinSearch.jsx
@@ -7,25 +7,25 @@ const CoinSearch = ({coins}) => {
 
     const [searchText,setSearchText]=useState('')
   return (
-    <div>
-        <div>
-        <h1>Search Crypto </h1>
+    <div className='rounded-div my-4 font-Josefin' >
+        <div className='flex flex-col md:flex-row justify-between pt-4 pb-6 text-center md:text-right'>
+        <h1 className='text-2xl font-bold my-2 '>Search Crypto </h1>
         <form >
-            <input onChange={(e)=> setSearchText(e.target.value)} type="text" placeholder='Search A Coin' />
+            <input onChange={(e)=> setSearchText(e.target.value)} type="text" placeholder='Search A Coin' className='text-center w-full bg-primary border border-input px-4 py-2 rounded-2xl shadow-xl'/>
         </form>
         </div>
 
-        <table>
+        <table className='w-full border-collapse text-center'>
             <thead>
-                <tr>
-                    <th></th>
+                <tr className='border-b'>
+                    <th className='px-4'></th>
                     <th>#</th>
-                    <th>Coin</th>
+                    <th className='text-left'>Coin</th>
                     <th></th>
                     <th>Price</th>
                     <th>24h</th>
-                    <th>24h Volume</th>
-                    <th>Market</th>
+                    <th className='hidden md:table-cell'>24h Volume</th>
+                    <th className='hidden sm:table-cell'>Market</th>
                     <th>Last 7 Days</th>
                 </tr>
             </thead>
@@ -47,7 +47,7 @@ const CoinSearch = ({coins}) => {
             </tbody>
         </table>
     </div>
-  )
-}
+  );
+};
 
 export default CoinSearch
