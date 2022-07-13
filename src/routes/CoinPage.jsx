507aff6f1aef37d2d78cfafe7e40b4f68f026253
Amandeep <singh.amandeep5665@gmail.com>
2022-07-13 15:20:33 +0530

    coinpage unstyled

--- src/routes/CoinPage.jsx
+++ src/routes/CoinPage.jsx
@@ -1,7 +1,14 @@
-import React,{useState,useEffect} from 'react'
+import React,{useState,useEffect,useContext} from 'react'
 import axios from 'axios'
+import { SparklinesLine } from 'react-sparklines';
+import { Sparklines,Sparkline } from 'react-sparklines';
+import { ThemeContext } from '../ThemeContext';
+import { FaTwitter,FaFacebook,FaReddit,FaGithub } from 'react-icons/fa';
+import DOMPurify from 'dompurify';
+
 const CoinPage = () => {
 
+    const {theme,setTheme} = useContext(ThemeContext)
     const[coin,setCoin]=useState({})
 
     const url='https://api.coingecko.com/api/v3/coins/bitcoin?localization=false&sparkline=true'
@@ -20,7 +27,110 @@ const CoinPage = () => {
                 <p>
                     {coin?.name} Price
                 </p>
+                <p>({coin.symbol?.toUpperCase()} /USD )</p>
             </div>
+        </div>
+
+        <div>
+            <div> 
+                <div>
+                    {coin.market_data?.current_price ? (<p>${coin.market_data.current_price.usd.toLocaleString()}</p>
+                    ) : null}
+                    <p>7 Day </p>
+                </div>
+                <div>
+                    <Sparklines data={coin.market_data?.sparkline_7d.price}>
+                    {theme==='dark' ? (<SparklinesLine color="#E79E4F" />) :
+                (<SparklinesLine color="#d06d6d
+                " />)}
+                    </Sparklines>
+                </div>
+
+                <div>
+                    <div>
+                        <p>Market Cap</p>
+                        {coin.market_data?.market_cap ? (<p>${coin.market_data?.market_cap.usd.toLocaleString()}</p>) : null}
+                    </div>
+
+                    <div>
+                        <p>Volume (24h)</p>
+                        {coin.market_data?.market_cap ? (<p>${coin.market_data?.total_volume.usd.toLocaleString()}</p>) : null}
+                        
+                    </div>
+                </div>
+
+                <div>
+                    <div>
+                        <p>24H High</p>
+                        {coin.market_data?.high_24h ? (<p>${coin.market_data?.high_24h.usd.toLocaleString()}</p>) : null}
+                    </div>
+                    <div>
+                        <p>24H Low</p>
+                        {coin.market_data?.low_24h ? (<p>${coin.market_data?.low_24h.usd.toLocaleString()}</p>) : null}
+                    </div>
+                </div>
+            </div>
+
+                <div>
+                    <p>Market Stats</p>
+                    <div>
+                        <div>
+                            <p>Market Rank</p>
+                            {coin.market_cap_rank}
+                        </div>
+                        <div>
+                            <p>Hashing Algorithm</p>
+                            {coin.hashing_algorithm ? (<p>{coin.hashing_algorithm}</p>) : null}
+                        </div>
+                        <div>
+                        <p>Trust Score</p>
+                        {coin.tickers ? (<p>{coin.liquidity_score.toFixed(2)}</p>) : null}
+                        </div>
+                    </div>
+
+                    <div>
+                        <div>
+                            <p>Price Change(24h)</p>
+                            {coin.market_data ? (<p>{coin.market_data.price_change_percentage_24h.toFixed(2)}%</p>) : null}
+                        </div>
+                        <div>
+                            <p>Price Change(7d)</p>
+                            {coin.market_data ? (<p>{coin.market_data.price_change_percentage_7d.toFixed(2)}%</p>) : null}
+                        </div>
+                        <div>
+                            <p>Price Change(14d)</p>
+                            {coin.market_data ? (<p>{coin.market_data.price_change_percentage_14d.toFixed(2)}%</p>) : null}
+                        </div>
+                    </div>
+
+                    <div>
+                        <div>
+                            <p>Price Change(30d)</p>
+                            {coin.market_data ? (<p>{coin.market_data.price_change_percentage_30d.toFixed(2)}%</p>) : null}
+                        </div>
+                        <div>
+                            <p>Price Change(60d)</p>
+                            {coin.market_data ? (<p>{coin.market_data.price_change_percentage_60d.toFixed(2)}%</p>) : null}
+                        </div>
+                        <div>
+                            <p>Price Change(1y)</p>
+                            {coin.market_data ? (<p>{coin.market_data.price_change_percentage_1y.toFixed(2)}%</p>) : null}
+                        </div>
+                    </div>
+
+                    <div>
+                        <FaTwitter />
+                        <FaFacebook />
+                        <FaReddit />
+                        <FaGithub />
+
+                    </div>
+                </div>
+        </div>
+
+        <div>
+            <p>About {coin.name}</p>
+            <p dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(coin.description ? coin.description.en : ''),}}></p>
         </div>
     </div>
   )
