0256524a2700260952176807d1c51f3faaf14e01
Amandeep <singh.amandeep5665@gmail.com>
2022-07-20 15:51:57 +0530

    sign in working

--- src/routes/Account.jsx
+++ src/routes/Account.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 import UserList from '../components/UserList'
+import { UserAuth } from '../context/AuthContext'
+
 
 const Account = () => {
+
+  const {user,logout} =UserAuth();
+  const navigate = useNavigate()
+
+  const handleSignOut = async () => {
+    try{
+      await logout()
+      navigate('/')
+    }
+    catch (e){
+      console.log(e.message)
+    }
+  }
   return (
     <div className='max-w-[1140px] mx-auto font-Josefin'>
       <div className='flex justify-between items-center py-8 my-12 rounded-div'>
@@ -9,13 +25,13 @@ const Account = () => {
           <h1 className='text-4xl font-bold'>Account</h1>
           <div>
             <p>
-              Welcome, User
+              Welcome, {user?.email}
             </p>
           </div>
         </div>
 
         <div>
-          <button className='border px-6 py-2 rounded-2xl shadow-lg hover:shadow-3xl hover:text-accent'>Sign Out</button>
+          <button onClick={handleSignOut} className='border px-6 py-2 rounded-2xl shadow-lg hover:shadow-3xl hover:text-accent'>Sign Out</button>
         </div>
       </div>
       <div className='flex justify-between items-center my-12 py-8 rounded-div'>
--- src/routes/Signin.jsx
+++ src/routes/Signin.jsx
@@ -1,23 +1,50 @@
 import React from 'react'
 import { AiFillLock, AiOutlineMail } from 'react-icons/ai'
-import { Link } from 'react-router-dom'
+import { Link,useNavigate,Navigate } from 'react-router-dom'
+import {signIn,UserAuth} from '../context/AuthContext'
+import { useState } from 'react'
 const Signin = () => {
+  const [email,setEmail] = useState('')
+  const [password,setPassword] = useState('')
+  const [error,setError] = useState('')
+  const {signIn}=UserAuth() 
+  const navigate = useNavigate();
+  const handleSubmit = async (e) =>
+  {
+    e.preventDefault()
+    setError('')
+    try{
+        await signIn(email,password);
+        navigate('/account')
+    }
+    catch (e) {
+      setError(e.message)
+      console.log(e.message)
+    }
+
+  }
+
+
   return (
     <div>
       <div className='max-w-[400px] mx-auto min-h-[600px] px-4 py-20 font-Josefin'>
         <h1 className='text-4xl font-bold'>Sign In</h1>
-        <form >
+        <form onSubmit={handleSubmit}>
           <div className='my-4 text-lg'>
             <label>Email</label>
             <div className='my-2 w-full relative rounded-2xl '>
-              <input className='w-full p-2 bg-primary border border-input rounded-2xl shadow-xl' type="email" />
+              <input 
+              onChange={(e)=>setEmail(e.target.value)}
+              className='w-full p-2 bg-primary border border-input rounded-2xl shadow-xl' type="email" />
               <AiOutlineMail className='absolute right-2 top-3 text-gray-400' />
             </div>
           </div>
           <div className='my-4 text-lg'>
             <label>Password</label>
             <div className='my-2 w-full relative rounded-2xl '>
-              <input className='w-full p-2 bg-primary border border-input rounded-2xl shadow-xl' type="password" />
+              <input 
+              onChange={(e)=>setPassword(e.target.value)}
+              className='w-full p-2 bg-primary border border-input rounded-2xl shadow-xl' type="password" />
               <AiFillLock className='absolute right-2 top-3 text-gray-400' />
             </div>
           </div>
